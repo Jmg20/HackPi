8c210098d01736ac3d735e8ae09b682a47db6817
Jmg20 <36897079+Jmg20@users.noreply.github.com>
2018-06-25 11:52:27 +0200

    Eliminación de las actualizaciones

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ KERNEL_VERSION=$(uname -r)
 MODULE_INSTALLED=false
 
 # Update Packages and ensure dependencies are installed
-sudo apt-get update
-sudo apt-get dist-upgrade -y
+#sudo apt-get update
+#sudo apt-get dist-upgrade -y
 sudo apt-get install -y isc-dhcp-server dsniff screen nodejs bridge-utils git
 sudo git clone https://github.com/samyk/poisontap ~/poisontap
 #sudo git clone https://github.com/lgandx/Responder ~/Responder
